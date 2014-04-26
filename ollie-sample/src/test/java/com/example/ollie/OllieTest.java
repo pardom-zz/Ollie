357570b9d058a198b13be4aa13a114f5b1503b5b
Michael Pardo <pardom@mobiata.com>
2014-04-26 13:07:22 -0400

    Delete old sqlite db before running tests.

--- ollie-sample/src/test/java/com/example/ollie/OllieTest.java
+++ ollie-sample/src/test/java/com/example/ollie/OllieTest.java
@@ -4,17 +4,25 @@ import com.example.ollie.model.Note;
 import com.example.ollie.shadows.PersistentShadowSQLiteOpenHelper;
 import ollie.Ollie;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import java.io.File;
+
 import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(manifest = Config.NONE, shadows = PersistentShadowSQLiteOpenHelper.class)
 public class OllieTest {
+	@BeforeClass
+	public static void preInitialize() {
+		new File("path").delete();
+	}
+
 	@Before
 	public void initialize() {
 		Ollie.init(Robolectric.application, "OllieSample.db", 1);
