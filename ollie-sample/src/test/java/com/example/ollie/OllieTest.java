c30f76022b6ff43857d66ab826d3c6c6cbd00372
Michael Pardo <pardom@mobiata.com>
2014-04-29 09:43:33 -0400

    Add delete test.

--- ollie-sample/src/test/java/com/example/ollie/OllieTest.java
+++ ollie-sample/src/test/java/com/example/ollie/OllieTest.java
@@ -5,6 +5,7 @@ import com.example.ollie.content.OllieSampleProvider;
 import com.example.ollie.model.Note;
 import com.example.ollie.shadows.PersistentShadowSQLiteOpenHelper;
 import ollie.Ollie;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -22,7 +23,12 @@ import static org.fest.assertions.api.Assertions.assertThat;
 @Config(manifest = Config.NONE, shadows = PersistentShadowSQLiteOpenHelper.class)
 public class OllieTest {
 	@BeforeClass
-	public static void preInitialize() {
+	public static void setup() {
+		new File("path").delete();
+	}
+
+	@AfterClass
+	public static void cleanUp() {
 		new File("path").delete();
 	}
 
@@ -57,5 +63,19 @@ public class OllieTest {
 		assertThat(note).isNotNull();
 		assertThat(note.id).isNotNull();
 		assertThat(note.id).isGreaterThan(0l);
+	}
+
+	@Test
+	public void testDeleteEntity() {
+		Note note = Note.find(Note.class, 1l);
+
+		assertThat(note).isNotNull();
+		assertThat(note.id).isNotNull();
+		assertThat(note.id).isGreaterThan(0l);
+
+		note.delete();
+
+		assertThat(note).isNotNull();
+		assertThat(note.id).isNull();
 	}
 }
